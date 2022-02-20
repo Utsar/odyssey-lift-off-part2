58d94b8cff23eb605d0373f2d67932b63057a507
Kristian Utsar <utsaru@gmail.com>
2022-02-20 00:12:24 +0000

    adds track page and navigation

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,2 +1,43 @@
 import { gql, useQuery } from "@apollo/client";
-import { Layout, Query } from "../components";
+import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
+
+export const GET_TRACK = gql`
+  query getTracks($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      description
+      modules {
+        id
+        title
+        length
+      }
+    }
+  }
+`;
+
+const Track = ({ trackId }) => {
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
+};
+
+export default Track;
